ecc8ea4821cca0602551c26feb198d4e4330aff7
PatriziaT <patrizia.trammell@yahoo.com>
2018-06-12 15:13:49 -0700

    imgs broken

--- src/pages/specials/specials.ts
+++ src/pages/specials/specials.ts
@@ -1,15 +1,9 @@
-
-import { Component, OnInit, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
-import { RVs } from '../../shared/rvs';
-import { RVsProvider } from '../../providers/rvs/rvs';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+//import { RVs } from '../../shared/rvs';
+//import { RVsProvider } from '../../providers/rvs/rvs';
 //import { DishdetailPage } from '../dishdetail/dishdetail';
-/**
- * Generated class for the SpecialsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
